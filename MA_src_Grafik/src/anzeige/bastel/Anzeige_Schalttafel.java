d4308e43a01ee72ce231cf7234bfb2d1384cb5d6
silence Laptop <silence8@gmx.de>
2014-01-14 17:19:56 +0100

    schalttafel

---
+++ MA_src_Grafik/src/anzeige/bastel/Anzeige_Schalttafel.java
@@ -0,0 +1,71 @@
+package anzeige.bastel;
+
+import schalttafel.Schalttafel;
+import schalttafel.anzeige.AnzeigerLampe;
+import schalttafel.artefakte.Artefakt;
+import schalttafel.artefakte.Hebel1;
+
+import anzeige.Anzeige;
+
+import com.jme3.input.KeyInput;
+import com.jme3.input.controls.KeyTrigger;
+import com.jme3.light.DirectionalLight;
+import com.jme3.math.Vector3f;
+
+public class Anzeige_Schalttafel extends Anzeige {
+
+	Schalttafel hebel = new Schalttafel();
+	
+
+	long lasttime = System.currentTimeMillis();
+	long deltaTime = 0;
+
+	boolean toggle = false;
+
+	public static void main(String[] args) {
+		Anzeige_Schalttafel app = new Anzeige_Schalttafel();
+		app.start();
+	}
+
+	@Override
+	public void simpleInitApp() {
+
+		cam.setLocation(new Vector3f(0f, 0f, 5f));
+
+
+		rootNode.attachChild(hebel.init(physic, assetManager, new Vector3f(0, 0, 0)));
+
+		
+
+
+		/** You must add a light to make the model visible */
+		DirectionalLight sun = new DirectionalLight();
+		sun.setDirection(new Vector3f(-0.1f, -0.7f, -1.0f).normalizeLocal());
+		rootNode.addLight(sun);
+	}
+
+	@Override
+	public void simpleUpdate(float tpf) {
+		// TODO Auto-generated method stub
+		super.simpleUpdate(tpf);
+		refreshTime();
+
+		for (Artefakt arte : artefakte) {
+			arte.update(deltaTime);
+		}
+	}
+
+	private void refreshTime() {
+		long time = System.currentTimeMillis();
+		deltaTime = time - lasttime;
+		lasttime = time;
+	}
+
+	
+
+	public void onAnalog(String binding, float value, float tpf) {
+
+		
+	}
+
+}
--- MA_src_Grafik/src/schalttafel/Schalttafel.java
+++ MA_src_Grafik/src/schalttafel/Schalttafel.java
@@ -1,5 +1,47 @@
 package schalttafel;
 
+import java.util.List;
+
+import schalttafel.artefakte.MyMaterial;
+
+import com.jme3.asset.AssetManager;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
+
 public class Schalttafel {
+	
+	private Node graficObject;
+	
+	private MyMaterial material;
+
+	public Node init(boolean physik, AssetManager assetManager, Vector3f position) {
+
+		
+		/** Load a teapot model (OBJ file from test-data) */
+		graficObject = (Node) assetManager.loadModel("obj/schalttafel/schalttafel1.obj");
+
+		List<Spatial> childs = graficObject.getChildren();
+		for (Spatial spatial : childs) {
+			System.out.println(spatial);
+		}
+
+		// Geometry g = (Geometry) graficObject.getChild("hebel1-geom-1");
+		// Material mat_default = new Material(assetManager,
+		// "Common/MatDefs/Misc/Unshaded.j3md");
+		// mat_default.setColor("Color", new ColorRGBA(0.5f, 0.5f, 0.5f, 1f));
+		// g.setMaterial(mat_default);
+
+		Geometry g = (Geometry) graficObject.getChild("schalttafel1-geom-0");
+		material = new MyMaterial(g.getMaterial());
+
+		graficObject.setLocalTranslation(position);
+		
+
+		return graficObject;
+	}
+
+
 
 }
