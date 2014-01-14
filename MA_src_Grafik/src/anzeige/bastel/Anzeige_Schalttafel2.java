1a2a63dc41e29d260b069f6335ecbfd457f324f2
silence Laptop <silence8@gmx.de>
2014-01-14 19:17:50 +0100

    h

--- MA_src_Grafik/src/anzeige/bastel/Anzeige_Schalttafel2.java
+++ MA_src_Grafik/src/anzeige/bastel/Anzeige_Schalttafel2.java
@@ -1,17 +1,24 @@
 package anzeige.bastel;
 
 import schalttafel.Schalttafel;
+import schalttafel.anzeige.AnzeigerLampe;
+import schalttafel.anzeige.AnzeigerStab;
 import schalttafel.artefakte.Artefakt;
+import schalttafel.artefakte.Hebel1;
+import schalttafel.artefakte.Rad1;
+
 import anzeige.Anzeige;
 
+import com.jme3.input.KeyInput;
+import com.jme3.input.controls.KeyTrigger;
 import com.jme3.light.DirectionalLight;
 import com.jme3.math.Vector3f;
 
-public class Anzeige_Schalttafel extends Anzeige {
+public class Anzeige_Schalttafel2 extends Anzeige {
 
 	Schalttafel schalttafel = new Schalttafel();
-	
-	
+	Rad1 rad1 = new Rad1();
+	AnzeigerStab as1 = new AnzeigerStab();
 
 	long lasttime = System.currentTimeMillis();
 	long deltaTime = 0;
@@ -19,7 +26,7 @@ public class Anzeige_Schalttafel extends Anzeige {
 	boolean toggle = false;
 
 	public static void main(String[] args) {
-		Anzeige_Schalttafel app = new Anzeige_Schalttafel();
+		Anzeige_Schalttafel2 app = new Anzeige_Schalttafel2();
 		app.start();
 	}
 
@@ -28,11 +35,17 @@ public class Anzeige_Schalttafel extends Anzeige {
 
 		cam.setLocation(new Vector3f(0f, 0f, 5f));
 
-
 		rootNode.attachChild(schalttafel.init(physic, assetManager, new Vector3f(0, 0, 0)));
 
-		
+		rootNode.attachChild(rad1.init(physic, assetManager, new Vector3f(0, 0, 0)));
+		rootNode.attachChild(as1.init(physic, assetManager, new Vector3f(0, 1, 0)));
+		rad1.setAnzeiger(as1);
 
+		handRechts.init(physic, assetManager, new Vector3f(1, 0, 0.2f));
+		// rootNode.attachChild(handLinks.init(physic, assetManager, new
+		// Vector3f(-1, 0, 0.2f)));
+
+		artefakte.add(rad1);
 
 		/** You must add a light to make the model visible */
 		DirectionalLight sun = new DirectionalLight();
@@ -57,11 +70,8 @@ public class Anzeige_Schalttafel extends Anzeige {
 		lasttime = time;
 	}
 
-	
-
 	public void onAnalog(String binding, float value, float tpf) {
 
-		
 	}
 
 }
