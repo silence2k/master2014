48f0f92bdbd7838f3297037ea8cf3e7d2b60b1a6
Olaf <silence8@gmx.de>
2014-01-28 23:45:09 +0100

    i

--- MA_src_Grafik/src/hud/steuerung/SpaceCoreSteuerungImpl.java
+++ MA_src_Grafik/src/hud/steuerung/SpaceCoreSteuerungImpl.java
@@ -2,18 +2,12 @@ package hud.steuerung;
 
 import schalttafel.anzeige.IF_Anzeiger;
 
-public class SpaceCoreSteuerungImpl extends SpaceCoreSteuerung{
-	
+public class SpaceCoreSteuerungImpl extends SpaceCoreSteuerung {
+
 	private JoyStickAnzeiger jsa = new JoyStickAnzeiger();
 	private SchieberAnzeiger sa = new SchieberAnzeiger();
 	private HebelAnzeiger ha = new HebelAnzeiger();
-	
-	
-	
-	
-	
-	
-	
+
 	public JoyStickAnzeiger getJoyStickAnzeiger() {
 		return jsa;
 	}
@@ -26,45 +20,44 @@ public class SpaceCoreSteuerungImpl extends SpaceCoreSteuerung{
 		return ha;
 	}
 
-	protected String getSchub(){
+	protected String getSchub() {
 		return Float.toString(schub);
 	}
-	
-//	String hoch = "s";
-//	String runter = "w";
-//	String links = "a";
-//	String rechts = "d";
-	
-	protected String getFahrgestell(){
-		return fahrwerkEingezogen?"-h":"h";
+
+	// String hoch = "s";
+	// String runter = "w";
+	// String links = "a";
+	// String rechts = "d";
+
+	protected String getFahrgestell() {
+		return fahrwerkEingezogen ? "-h" : "h";
 	}
-	
-	protected String getLinks(){
-		return links?"a":"-a";
+
+	protected String getLinks() {
+		return links ? "a" : "-a";
 	}
-	
-	protected String getRechts(){
-		return rechts?"d":"-d";
+
+	protected String getRechts() {
+		return rechts ? "d" : "-d";
 	}
-	
-	protected String getHoch(){
-		return hoch?"s":"-s";
+
+	protected String getHoch() {
+		return hoch ? "s" : "-s";
 	}
-	
-	protected String getRunter(){
-		return runter?"w":"-w";
+
+	protected String getRunter() {
+		return runter ? "w" : "-w";
 	}
-	
-	
+
 	/*
-	 *     - 
-	 *   1 2 3 
-	 * + 4   5 - 
-	 *   6 7 8 
-	 *     +
+	 * .....-
+	 * ...1 2 3
+	 *  + 4...5 -
+	 * ...6 7 8
+	 * .... +
 	 */
-	
-	class JoyStickAnzeiger implements IF_Anzeiger{
+
+	class JoyStickAnzeiger implements IF_Anzeiger {
 
 		@Override
 		public void setZielWert(float zielwert) {
@@ -72,31 +65,31 @@ public class SpaceCoreSteuerungImpl extends SpaceCoreSteuerung{
 			links = false;
 			rechts = false;
 			hoch = false;
-			
-			if(zielwert == 1f || zielwert == 2f || zielwert == 3f){
+
+			if (zielwert == 1f || zielwert == 2f || zielwert == 3f) {
 				runter = true;
 			}
-			if(zielwert == 1f || zielwert == 4f || zielwert == 6f){
+			if (zielwert == 1f || zielwert == 4f || zielwert == 6f) {
 				links = true;
 			}
-			if(zielwert == 3f || zielwert == 5f || zielwert == 8f){
+			if (zielwert == 3f || zielwert == 5f || zielwert == 8f) {
 				rechts = true;
 			}
-			if(zielwert == 6f || zielwert == 7f || zielwert == 8f){
+			if (zielwert == 6f || zielwert == 7f || zielwert == 8f) {
 				hoch = true;
 			}
-			
+
 		}
 
 		@Override
 		public void update(float deltaTime) {
 			// TODO Auto-generated method stub
-			
+
 		}
-		
+
 	}
-	
-	class SchieberAnzeiger implements IF_Anzeiger{
+
+	class SchieberAnzeiger implements IF_Anzeiger {
 
 		@Override
 		public void setZielWert(float zielwert) {
@@ -106,25 +99,25 @@ public class SpaceCoreSteuerungImpl extends SpaceCoreSteuerung{
 		@Override
 		public void update(float deltaTime) {
 			// TODO Auto-generated method stub
-			
+
 		}
-		
+
 	}
-	
-	class HebelAnzeiger implements IF_Anzeiger{
+
+	class HebelAnzeiger implements IF_Anzeiger {
 
 		@Override
 		public void setZielWert(float zielwert) {
 			// TODO Auto-generated method stub
-			
+
 		}
 
 		@Override
 		public void update(float deltaTime) {
 			// TODO Auto-generated method stub
-			
+
 		}
-		
+
 	}
 
 }
