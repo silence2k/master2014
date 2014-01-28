99bbbcd0d5aa5aa9fd288d24a9001ea778c10d89
Olaf <silence8@gmx.de>
2014-01-28 22:43:16 +0100

    s

--- MA_src_Game/src/spacecore/PlayerShip.java
+++ MA_src_Game/src/spacecore/PlayerShip.java
@@ -241,20 +241,20 @@ public class PlayerShip {
 
 		GL11.glMultMatrix(Buffer);
 
-		GL11.glLineWidth(2.0f);
-		GL11.glBegin(GL11.GL_LINES);
-		GL11.glColor3f(1, 0.5f, 0.5f);
-		GL11.glVertex3f(0, 0, 0);
-		GL11.glVertex3f(1, 0, 0);
-
-		GL11.glColor3f(0.5f, 1, 0.5f);
-		GL11.glVertex3f(0, 0, 0);
-		GL11.glVertex3f(0, 1, 0);
-
-		GL11.glColor3f(0.5f, 0.5f, 1);
-		GL11.glVertex3f(0, 0, 0);
-		GL11.glVertex3f(0, 0, 1);
-		GL11.glEnd();
+//		GL11.glLineWidth(2.0f);
+//		GL11.glBegin(GL11.GL_LINES);
+//		GL11.glColor3f(1, 0.5f, 0.5f);
+//		GL11.glVertex3f(0, 0, 0);
+//		GL11.glVertex3f(1, 0, 0);
+//
+//		GL11.glColor3f(0.5f, 1, 0.5f);
+//		GL11.glVertex3f(0, 0, 0);
+//		GL11.glVertex3f(0, 1, 0);
+//
+//		GL11.glColor3f(0.5f, 0.5f, 1);
+//		GL11.glVertex3f(0, 0, 0);
+//		GL11.glVertex3f(0, 0, 1);
+//		GL11.glEnd();
 
 		// Set width to a single line
 		GL11.glLineWidth(1);
